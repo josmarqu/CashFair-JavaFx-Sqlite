28ad13f5915565c3e830330362faa1030681e536
josemarquezmontoro <josemarquezmontoro@hotmail.com>
2022-12-07 12:34:58 +0100

    base project directory structure

--- src/main/java/com/example/cashfair/App.java
+++ src/main/java/com/example/cashfair/App.java
@@ -2,21 +2,25 @@ package com.example.cashfair;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
+    private static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        scene = new Scene(loadFXML("hello-view"), 1366, 768);
         stage.setScene(scene);
         stage.show();
     }
 
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
     public static void main(String[] args) {
         launch();
     }
