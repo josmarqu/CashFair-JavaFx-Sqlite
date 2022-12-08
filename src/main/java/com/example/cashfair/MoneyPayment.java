f9a0cf41d1990f4cd9dffd2772c5eb5b45b75c87
JunZhou <zhoujun97099@gmail.com>
2022-12-08 19:59:39 +0100

    fixed resizing issue in UI, implemented controller and main class for MoneyPayment screen

--- src/main/java/com/example/cashfair/MoneyPayment.java
+++ src/main/java/com/example/cashfair/MoneyPayment.java
@@ -4,21 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class App extends Application {
+public class MoneyPayment extends Application {
+
     private static Scene scene;
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("history-screen"), 1366, 768);
+        scene = new Scene(loadFXML("money-payment"), 1000, 500);
         stage.setScene(scene);
         stage.setResizable(true);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
+    private static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
@@ -38,3 +40,4 @@ public class App extends Application {
         launch();
     }
 }
+
--- src/main/java/com/example/cashfair/views/MoneyPaymentController.java
+++ src/main/java/com/example/cashfair/views/MoneyPaymentController.java
@@ -1,4 +1,34 @@
 package com.example.cashfair.views;
 
-public class MoneyPaymentController {
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MoneyPaymentController implements Initializable {
+
+    @FXML
+    private ComboBox moneyPaymentScrp;
+
+    @FXML
+    private Spinner<Integer> moneyPaymentSpnr;
+
+    @FXML
+    private TableView moneyPaymentLstVw;
+
+    @FXML
+    private Button btnApply;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        moneyPaymentSpnr.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 0));
+        moneyPaymentSpnr.setEditable(true);
+
+
+    }
 }
