99f73e70839055a8aa562e83b87fddd6363375d2
josemarquezmontoro <josemarquezmontoro@hotmail.com>
2022-12-21 22:04:22 +0100

    button change style css fixed

--- src/main/java/com/controllers/HomeScreenController.java
+++ src/main/java/com/controllers/HomeScreenController.java
@@ -16,19 +16,24 @@ public class HomeScreenController {
     public void initialize() {
         btnPay.setOnAction(event -> App.redirectTo("addContributor"));
         btnHis.setOnAction(event -> App.redirectTo("history-screen"));
-        btnStyle.setGraphic(App.DARKIMG);
         btnStyle.setOnAction(event -> changeStyle());
+        if (App.style .contains("light_mode")){
+            btnStyle.setGraphic(App.DARKIMG);
+        } else {
+            btnStyle.setGraphic(App.LIGHTIMG);
+        }
     }
 
     private void changeStyle() {
         if (App.style.contains("light_mode")){
             App.style = "dark_mode";
-            btnStyle.setGraphic(App.LIGHTIMG);
             App.changeStyleSheet();
+            btnStyle.setGraphic(App.LIGHTIMG);
+
         } else {
             App.style = "light_mode";
-            btnStyle.setGraphic(App.DARKIMG);
             App.changeStyleSheet();
+            btnStyle.setGraphic(App.DARKIMG);
         }
     }
 }
